99b4f25e28102879195e55eeb1f5c4f3a41636aa
Risingfeanyx <35235934+Risingfeanyx@users.noreply.github.com>
2020-07-12 04:21:11 -0400

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,273 @@
+#!/bin/bash
+# A menu driven shell script sample template 
+## ----------------------------------
+# Step #1: Define variables
+# ----------------------------------
+EDITOR=nano
+PASSWD=/etc/passwd
+RED='\033[0;41;30m'
+STD='\033[0;0;39m'
+ 
+# ----------------------------------
+# Step #2: User defined function
+# ----------------------------------
+pause(){
+  read -p "Press [Enter] key to continue..." fackEnterKey
+}
+
+one(){
+	open https://d3bxy9euw4e147.cloudfront.net/oscms-prodcms/media/documents/AlgebraAndTrigonometry-OP_NpLRuqX.pdf
+
+        pause
+}
+ 
+two(){
+	open https://d3bxy9euw4e147.cloudfront.net/oscms-prodcms/media/documents/AmericanGovernment2e-OP_WpMZUKx.pdf
+        pause
+}
+three(){
+        open https://d3bxy9euw4e147.cloudfront.net/oscms-prodcms/media/documents/AnatomyandPhysiology-OP_xxKIcSo.pdf
+        pause
+}
+
+four(){
+	open https://d3bxy9euw4e147.cloudfront.net/oscms-prodcms/media/documents/Astronomy-OP_oV0J80E.pdf
+        pause
+}
+
+five(){
+        open https://d3bxy9euw4e147.cloudfront.net/oscms-prodcms/media/documents/Biology2e-WEB_eUNkk2q.pdf
+
+        pause
+}
+
+six(){
+        open https://d3bxy9euw4e147.cloudfront.net/oscms-prodcms/media/documents/BusinessEthics-OP_BQNN2qf.pdf
+        pause
+}
+
+seven(){
+	open https://d3bxy9euw4e147.cloudfront.net/oscms-prodcms/media/documents/Calculus_Volume_1_-_WEB.pdf
+        pause
+}
+
+eight(){
+	open https://d3bxy9euw4e147.cloudfront.net/oscms-prodcms/media/documents/CalculusVolume2-OP_esPpXTB.pdf
+        pause
+}
+
+nine(){
+        open https://d3bxy9euw4e147.cloudfront.net/oscms-prodcms/media/documents/CalculusVolume3-OP_n7Nj74c.pdf
+        pause
+}
+
+ten(){
+	open https://d3bxy9euw4e147.cloudfront.net/oscms-prodcms/media/documents/Chemistry2e-OP_ZIUdkjH.pdf
+        pause
+}
+
+eleven(){
+        open https://d3bxy9euw4e147.cloudfront.net/oscms-prodcms/media/documents/ChemistryAtomsFirst2e-OP_T2wT7wj.pdf
+        pause
+}
+
+twelve(){
+	open https://d3bxy9euw4e147.cloudfront.net/oscms-prodcms/media/documents/CollegeAlgebra-OP_B7PUoNW.pdf
+        pause
+}
+
+thirteen(){
+	open https://d3bxy9euw4e147.cloudfront.net/oscms-prodcms/media/documents/College_Physics-WEB.pdf
+        pause
+}
+
+fourteen(){
+        open https://d3bxy9euw4e147.cloudfront.net/oscms-prodcms/media/documents/College_Success_web_slqpdO2.pdf
+        pause
+}
+fifteen(){
+	open https://d3bxy9euw4e147.cloudfront.net/oscms-prodcms/media/documents/Economics2e-OP_I2ne43X.pdf
+        pause
+}
+sixteen(){
+	open https://d3bxy9euw4e147.cloudfront.net/oscms-prodcms/media/documents/Entrepreneurship-WEB.pdf
+        pause
+}
+seventeen(){
+	open https://d3bxy9euw4e147.cloudfront.net/oscms-prodcms/media/documents/FinancialAccounting-OP_dwsQeqn.pdf
+        pause
+}
+eighteen(){
+        open https://d3bxy9euw4e147.cloudfront.net/oscms-prodcms/media/documents/IntermediateAlgebra2e-WEB.pdf
+        pause
+}
+nineteen(){
+	open https://d3bxy9euw4e147.cloudfront.net/oscms-prodcms/media/documents/IntroductionToBusiness-OP_gPQqj30.pdf
+        pause
+}
+twenty(){
+        open https://d3bxy9euw4e147.cloudfront.net/oscms-prodcms/media/documents/IntroductionToSociology2e-OP_D8IepwZ.pdf
+        pause
+}
+twentyone(){
+        open https://d3bxy9euw4e147.cloudfront.net/oscms-prodcms/media/documents/IntroductoryStatistics-OP_LXn0jei.pdf
+        pause
+}
+twentytwo(){
+        open https://d3bxy9euw4e147.cloudfront.net/oscms-prodcms/media/documents/Macroeconomics2e-OP.pdf
+        pause
+}
+twentythree(){
+        open https://d3bxy9euw4e147.cloudfront.net/oscms-prodcms/media/documents/ManagerialAccounting-OP_os574CR.pdf
+        pause
+}
+twentyfour(){
+	open https://d3bxy9euw4e147.cloudfront.net/oscms-prodcms/media/documents/Microbiology-OP.pdf
+        pause
+}
+
+twentyfive(){
+        open https://d3bxy9euw4e147.cloudfront.net/oscms-prodcms/media/documents/Microeconomics2e-OP_603oHEs.pdf
+        pause
+}
+twentysix(){
+        open https://d3bxy9euw4e147.cloudfront.net/oscms-prodcms/media/documents/OrganizationalBehavior-OP.pdf
+        pause
+}
+twentyseven(){
+        open https://d3bxy9euw4e147.cloudfront.net/oscms-prodcms/media/documents/Prealgebra2e-WEB_yC5bGuI.pdf
+        pause
+}
+twentyeight(){
+        open https://d3bxy9euw4e147.cloudfront.net/oscms-prodcms/media/documents/Precalculus-OP_mbNC8an.pdf
+        pause
+}
+twentynine(){
+        open https://d3bxy9euw4e147.cloudfront.net/oscms-prodcms/media/documents/Prealgebra2e-WEB_yC5bGuI.pdf
+        pause
+}
+thirty(){
+        open https://d3bxy9euw4e147.cloudfront.net/oscms-prodcms/media/documents/Psychology-OP_F4YYkDr.pdf
+        pause
+}
+thirtyone(){
+        open https://d3bxy9euw4e147.cloudfront.net/oscms-prodcms/media/documents/USHistory-OP_IdjNctE.pdf
+        pause
+}
+thirtytwo(){
+        open https://d3bxy9euw4e147.cloudfront.net/oscms-prodcms/media/documents/UniversityPhysicsVolume1-OP_gUzvYbP.pdf
+
+        pause
+}
+thirtythree(){
+	open https://d3bxy9euw4e147.cloudfront.net/oscms-prodcms/media/documents/UniversityPhysicsVolume2-OP.pdf
+        pause
+}
+thirtyfour(){
+	open https://d3bxy9euw4e147.cloudfront.net/oscms-prodcms/media/documents/UniversityPhysicsVolume3-OP.pdf
+        pause
+}
+
+ 
+# function to display menus
+show_menus() {
+	clear
+	echo "~~~~~~~~~~~~~~~~~~~~~~~~~"	
+	echo " Select an OpenStax Book"
+	echo "~~~~~~~~~~~~~~~~~~~~~~~~~"
+	echo "1. Algebra and Trigonometry"
+	echo "2. American Government"
+	echo "3. Anatomy and Physiology"
+	echo "4. Astronomy"
+	echo "5. Biology 2"
+	echo "6. Business Ethics"
+	echo "7. Calculus Volume 1"
+	echo "8. Calculus Volume 2"
+	echo "9. Calculus Volume 3"
+	echo "10. Chemistry"
+	echo "11. Chemistry 2"
+	echo "12. College Algebra"
+	echo "13. College Physics" 
+	echo "14. College Success"
+	echo "15. Economics 2"
+	echo "16. Entrepreneurship"
+	echo "17. Financial Accounting"
+	echo "18. Intermediate Algebra"
+	echo "19. Introduction to Business"
+	echo "20. Introduction to Sociology 2"
+	echo "21. Introductory Statistics"
+	echo "22. Macroeconomics 2"
+	echo "23. Managerial Accounting"
+	echo "24. Microbiology"
+	echo "25. Microeconomics 2 "
+	echo "26. Organizational Behavior"
+	echo "27. Pre-Algebra 2" 
+	echo "28. Pre-Calculus"
+	echo "29. Principles of Management"
+	echo "30. Psychology"
+	echo "31. US History"
+	echo "32. University Physics Volume 1"
+	echo "33. University Physics Volume 2"
+	echo "34. University Physics Volume 3"
+}
+# read input from the keyboard and take a action
+# invoke the one() when the user select 1 from the menu option.
+# invoke the two() when the user select 2 from the menu option.
+# Exit when user the user select 3 form the menu option.
+read_options(){
+	local choice
+	read -p "Enter choice [ 1 - 35] " choice
+	case $choice in
+		1) one ;;
+		2) two ;;
+		3) three ;;
+		4) four ;;
+		5) five ;;
+		6) six ;;
+		7) seven ;;
+		8) eight ;;
+		9) nine ;;
+		10) ten ;;
+		11) eleven ;;
+		12) twelve ;;
+		13) thirteen ;;
+		14) fourteen ;;
+		15) fifteen ;; 
+		16) sixteen ;;
+		17) seventeen ;;
+		18) eighteen ;;
+		19) nineteen ;;
+		20) twenty ;;
+		21) twentyone ;;
+		22) twentytwo ;;
+		23) twentythree ;;
+		24) twentyfour ;;
+		25) twentyfive ;;
+		26) twentysix ;;
+		27) twentyseven ;;
+		28) twentyeight ;;
+		29) twentynine ;;
+		30) thirty ;;
+		31) thirtyone ;;
+		32) thirtytwo ;;
+		33) thirtythree ;; 
+		34) thirtyfour ;;
+		35) thirtyfive ;;
+		*) echo -e "${RED}Error...${STD}" && sleep 2
+	esac
+}
+ 
+# ----------------------------------------------
+# Step #3: Trap CTRL+C, CTRL+Z and quit singles
+# ----------------------------------------------
+#trap '' SIGINT SIGQUIT SIGTSTP
+ 
+# -----------------------------------
+# Step #4: Main logic - infinite loop
+# ------------------------------------
+while true
+do
+ 
+	show_menus
+	read_options
+done
