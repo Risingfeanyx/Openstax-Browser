9b89c0cff0ec9917a2d6f796d967a73a9d2456e5
Risingfeanyx <35235934+Risingfeanyx@users.noreply.github.com>
2021-08-17 04:56:54 -0400

    Update main.sh
    added $homeurl variable in order to change should parent domain change. Will convert to whiptail for cleanliness

--- main.sh
+++ main.sh
@@ -1,166 +1,201 @@
 #!/bin/bash
-EDITOR=nano
-PASSWD=/etc/passwd
-RED='\033[0;41;30m'
-STD='\033[0;0;39m'
+homeurl=https://assets.openstax.org/oscms-prodcms/media/documents
 
 pause(){
-  read -p "Press [Enter] key to continue..." fackEnterKey
+  read -pr "Press [Enter] key to continue..."
 }
 
 one(){
-	wget https://d3bxy9euw4e147.cloudfront.net/oscms-prodcms/media/documents/AlgebraAndTrigonometry-OP_NpLRuqX.pdf ; open AlgebraAndTrigonometry-OP_NpLRuqX.pdf
+	 wget $homeurl/AlgebraAndTrigonometry-OP_1tE6R5r.pdf -nc -O Algebra_Trig.pdf
+	xdg-open Algebra_Trig.pdf
         pause
 }
  
 two(){
-	wget https://d3bxy9euw4e147.cloudfront.net/oscms-prodcms/media/documents/AmericanGovernment2e-OP_WpMZUKx.pdf ; open AmericanGovernment2e-OP_WpMZUKx.pdf
+	wget $homeurl/AmericanGovernment2e-OP_WpMZUKx.pdf -nc ; xdg-open AmericanGovernment2e-OP_WpMZUKx.pdf
         pause
 }
 three(){
-        wget https://d3bxy9euw4e147.cloudfront.net/oscms-prodcms/media/documents/AnatomyandPhysiology-OP_xxKIcSo.pdf ; open AnatomyandPhysiology-OP_xxKIcSo.pdf
+        wget $homeurl/AnatomyandPhysiology-OP_xxKIcSo.pdf -nc ; xdg-open AnatomyandPhysiology-OP_xxKIcSo.pdf
         pause
 }
 
 four(){
-	wget https://d3bxy9euw4e147.cloudfront.net/oscms-prodcms/media/documents/Astronomy-OP_oV0J80E.pdf ; open Astronomy-OP_oV0J80E.pdf
+	wget $homeurl/Astronomy-OP_oV0J80E.pdf -nc ; xdg-open Astronomy-OP_oV0J80E.pdf
         pause
 }
 
 five(){
-        wget https://d3bxy9euw4e147.cloudfront.net/oscms-prodcms/media/documents/Biology2e-WEB_eUNkk2q.pdf ; open Biology2e-WEB_eUNkk2q.pdf
+        wget $homeurl/Biology2e-WEB_eUNkk2q.pdf -nc ; xdg-open Biology2e-WEB_eUNkk2q.pdf
 
         pause
 }
 
 six(){
-        wget https://d3bxy9euw4e147.cloudfront.net/oscms-prodcms/media/documents/BusinessEthics-OP_BQNN2qf.pdf ; open BusinessEthics-OP_BQNN2qf.pdf
+        wget $homeurl/BusinessEthics-OP_BQNN2qf.pdf -nc ; xdg-open BusinessEthics-OP_BQNN2qf.pdf
         pause
 }
 
 seven(){
-	wget https://d3bxy9euw4e147.cloudfront.net/oscms-prodcms/media/documents/Calculus_Volume_1_-_WEB.pdf ; open Calculus_Volume_1_-_WEB.pdf 
+	wget $homeurl/Calculus_Volume_1_-_WEB.pdf -nc ; xdg-open Calculus_Volume_1_-_WEB.pdf 
         pause
 }
 
 eight(){
-	wget https://d3bxy9euw4e147.cloudfront.net/oscms-prodcms/media/documents/CalculusVolume2-OP_esPpXTB.pdf ; open CalculusVolume2-OP_esPpXTB.pdf
+	wget $homeurl/CalculusVolume2-OP_esPpXTB.pdf -nc ; xdg-open CalculusVolume2-OP_esPpXTB.pdf
         pause
 }
 
 nine(){
-        wget https://d3bxy9euw4e147.cloudfront.net/oscms-prodcms/media/documents/CalculusVolume3-OP_n7Nj74c.pdf ; open CalculusVolume3-OP_n7Nj74c.pdf 
+        wget $homeurl/CalculusVolume3-OP_n7Nj74c.pdf -nc ; xdg-open CalculusVolume3-OP_n7Nj74c.pdf 
         pause
 }
 
 ten(){
-	wget https://d3bxy9euw4e147.cloudfront.net/oscms-prodcms/media/documents/Chemistry2e-OP_ZIUdkjH.pdf ; open Chemistry2e-OP_ZIUdkjH.pdf 
+	wget $homeurl/Chemistry2e-OP_ZIUdkjH.pdf -nc ; xdg-open Chemistry2e-OP_ZIUdkjH.pdf 
         pause
 }
 
 eleven(){
-        wget https://d3bxy9euw4e147.cloudfront.net/oscms-prodcms/media/documents/ChemistryAtomsFirst2e-OP_T2wT7wj.pdf ; open ChemistryAtomsFirst2e-OP_T2wT7wj.pdf
+        wget $homeurl/ChemistryAtomsFirst2e-OP_T2wT7wj.pdf -nc ; xdg-open ChemistryAtomsFirst2e-OP_T2wT7wj.pdf
         pause
 }
 
 twelve(){
-	wget https://d3bxy9euw4e147.cloudfront.net/oscms-prodcms/media/documents/CollegeAlgebra-OP_B7PUoNW.pdf ; open CollegeAlgebra-OP_B7PUoNW.pdf
+	wget $homeurl/CollegeAlgebra-OP_B7PUoNW.pdf -nc ; xdg-open CollegeAlgebra-OP_B7PUoNW.pdf
         pause
 }
 
 thirteen(){
-	wget https://d3bxy9euw4e147.cloudfront.net/oscms-prodcms/media/documents/College_Physics-WEB.pdf ; open College_Physics-WEB.pdf 
+	wget $homeurl/College_Physics-WEB.pdf -nc ; xdg-open College_Physics-WEB.pdf 
         pause
 }
 
 fourteen(){
-        wget https://d3bxy9euw4e147.cloudfront.net/oscms-prodcms/media/documents/College_Success_web_slqpdO2.pdf ; open College_Success_web_slqpdO2.pdf
+        wget $homeurl/College_Success_web_slqpdO2.pdf -nc ; xdg-open College_Success_web_slqpdO2.pdf
         pause
 }
 fifteen(){
-	wget https://d3bxy9euw4e147.cloudfront.net/oscms-prodcms/media/documents/Economics2e-OP_I2ne43X.pdf ; open Economics2e-OP_I2ne43X.pdf 
+	wget $homeurl/Economics2e-OP_I2ne43X.pdf -nc ; xdg-open Economics2e-OP_I2ne43X.pdf 
         pause
 }
 sixteen(){
-	wget https://d3bxy9euw4e147.cloudfront.net/oscms-prodcms/media/documents/Entrepreneurship-WEB.pdf ; open Entrepreneurship-WEB.pdf
+	wget $homeurl/Entrepreneurship-WEB.pdf -nc ; xdg-open Entrepreneurship-WEB.pdf
         pause
 }
 seventeen(){
-	wget https://d3bxy9euw4e147.cloudfront.net/oscms-prodcms/media/documents/FinancialAccounting-OP_dwsQeqn.pdf ; open FinancialAccounting-OP_dwsQeqn.pdf 
+	wget $homeurl/FinancialAccounting-OP_dwsQeqn.pdf -nc ; xdg-open FinancialAccounting-OP_dwsQeqn.pdf 
         pause
 }
 eighteen(){
-        wget https://d3bxy9euw4e147.cloudfront.net/oscms-prodcms/media/documents/IntermediateAlgebra2e-WEB.pdf ; open IntermediateAlgebra2e-WEB.pdf
+        wget $homeurl/IntermediateAlgebra2e-WEB.pdf -nc ; xdg-open IntermediateAlgebra2e-WEB.pdf
         pause
 }
 nineteen(){
-	wget https://d3bxy9euw4e147.cloudfront.net/oscms-prodcms/media/documents/IntroductionToBusiness-OP_gPQqj30.pdf ; open IntroductionToBusiness-OP_gPQqj30.pdf
+	wget $homeurl/IntroductionToBusiness-OP_gPQqj30.pdf -nc ; xdg-open IntroductionToBusiness-OP_gPQqj30.pdf
         pause
 }
 twenty(){
-        wget https://d3bxy9euw4e147.cloudfront.net/oscms-prodcms/media/documents/IntroductionToSociology2e-OP_D8IepwZ.pdf ; open IntroductionToSociology2e-OP_D8IepwZ.pdf
+        wget $homeurl/IntroductionToSociology2e-OP_D8IepwZ.pdf -nc ; xdg-open IntroductionToSociology2e-OP_D8IepwZ.pdf
         pause
 }
 twentyone(){
-        wget https://d3bxy9euw4e147.cloudfront.net/oscms-prodcms/media/documents/IntroductoryStatistics-OP_LXn0jei.pdf ; open IntroductoryStatistics-OP_LXn0jei.pdf
+        wget $homeurl/IntroductoryStatistics-OP_LXn0jei.pdf -nc ; xdg-open IntroductoryStatistics-OP_LXn0jei.pdf
         pause
 }
 twentytwo(){
-        wget https://d3bxy9euw4e147.cloudfront.net/oscms-prodcms/media/documents/Macroeconomics2e-OP.pdf ; open Macroeconomics2e-OP.pdf
+        wget $homeurl/Macroeconomics2e-OP.pdf -nc ; xdg-open Macroeconomics2e-OP.pdf
         pause
 }
 twentythree(){
-        wget https://d3bxy9euw4e147.cloudfront.net/oscms-prodcms/media/documents/ManagerialAccounting-OP_os574CR.pdf ; open ManagerialAccounting-OP_os574CR.pdf
+        wget $homeurl/ManagerialAccounting-OP_os574CR.pdf -nc ; xdg-open ManagerialAccounting-OP_os574CR.pdf
         pause
 }
 twentyfour(){
-	wget https://d3bxy9euw4e147.cloudfront.net/oscms-prodcms/media/documents/Microbiology-OP.pdf ; open Microbiology-OP.pdf
+	wget $homeurl/Microbiology-OP.pdf -nc ; xdg-open Microbiology-OP.pdf
         pause
 }
 
 twentyfive(){
-        wget https://d3bxy9euw4e147.cloudfront.net/oscms-prodcms/media/documents/Microeconomics2e-OP_603oHEs.pdf ; open Microeconomics2e-OP_603oHEs.pdf
+        wget $homeurl/Microeconomics2e-OP_603oHEs.pdf -nc ; xdg-open Microeconomics2e-OP_603oHEs.pdf
         pause
 }
 twentysix(){
-        wget https://d3bxy9euw4e147.cloudfront.net/oscms-prodcms/media/documents/OrganizationalBehavior-OP.pdf ; open OrganizationalBehavior-OP.pdf
+        wget $homeurl/OrganizationalBehavior-OP.pdf -nc ; xdg-open OrganizationalBehavior-OP.pdf
         pause
 }
 twentyseven(){
-        wget https://d3bxy9euw4e147.cloudfront.net/oscms-prodcms/media/documents/Prealgebra2e-WEB_yC5bGuI.pdf ; open Prealgebra2e-WEB_yC5bGuI.pdf
+        wget $homeurl/Prealgebra2e-WEB_yC5bGuI.pdf -nc ; xdg-open Prealgebra2e-WEB_yC5bGuI.pdf
         pause
 }
 twentyeight(){
-        wget https://d3bxy9euw4e147.cloudfront.net/oscms-prodcms/media/documents/Precalculus-OP_mbNC8an.pdf ; open Precalculus-OP_mbNC8an.pdf
+        wget $homeurl/Precalculus-OP_mbNC8an.pdf -nc ; xdg-open Precalculus-OP_mbNC8an.pdf
         pause
 }
 twentynine(){
-        wget https://d3bxy9euw4e147.cloudfront.net/oscms-prodcms/media/documents/Prealgebra2e-WEB_yC5bGuI.pdf ; open Prealgebra2e-WEB_yC5bGuI.pdf
+        wget $homeurl/Prealgebra2e-WEB_yC5bGuI.pdf -nc ; xdg-open Prealgebra2e-WEB_yC5bGuI.pdf
         pause
 }
 thirty(){
-        wget https://d3bxy9euw4e147.cloudfront.net/oscms-prodcms/media/documents/Psychology-OP_F4YYkDr.pdf ; open Psychology-OP_F4YYkDr.pdf
+        wget $homeurl/Psychology-OP_F4YYkDr.pdf -nc ; xdg-open Psychology-OP_F4YYkDr.pdf
         pause
 }
 thirtyone(){
-        wget https://d3bxy9euw4e147.cloudfront.net/oscms-prodcms/media/documents/USHistory-OP_IdjNctE.pdf ; open USHistory-OP_IdjNctE.pdf
+        wget $homeurl/USHistory-OP_IdjNctE.pdf -nc ; xdg-open USHistory-OP_IdjNctE.pdf
         pause
 }
 thirtytwo(){
-        wget https://d3bxy9euw4e147.cloudfront.net/oscms-prodcms/media/documents/UniversityPhysicsVolume1-OP_gUzvYbP.pdf ; open UniversityPhysicsVolume1-OP_gUzvYbP.pdf
+        wget $homeurl/UniversityPhysicsVolume1-OP_gUzvYbP.pdf -nc ; xdg-open UniversityPhysicsVolume1-OP_gUzvYbP.pdf
 
         pause
 }
 thirtythree(){
-	wget https://d3bxy9euw4e147.cloudfront.net/oscms-prodcms/media/documents/UniversityPhysicsVolume2-OP.pdf ; open UniversityPhysicsVolume2-OP.pdf
+	wget $homeurl/UniversityPhysicsVolume2-OP.pdf -nc ; xdg-open UniversityPhysicsVolume2-OP.pdf
         pause
 }
 thirtyfour(){
-	wget https://d3bxy9euw4e147.cloudfront.net/oscms-prodcms/media/documents/UniversityPhysicsVolume3-OP.pdf ; open UniversityPhysicsVolume3-OP.pdf
+	wget $homeurl/UniversityPhysicsVolume3-OP.pdf -nc ; xdg-open UniversityPhysicsVolume3-OP.pdf
         pause
 }
 thirtyfive(){
-	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Risingfeanyx/Openstax-Browser/master/big_dl)" 
+
+wget -i -  << EOD
+$homeurl/AlgebraAndTrigonometry-OP_1tE6R5r.pdf
+$homeurl/AmericanGovernment2e-OP_WpMZUKx.pdf
+$homeurl/AnatomyandPhysiology-OP_xxKIcSo.pdf
+$homeurl/Astronomy-OP_oV0J80E.pdf
+$homeurl/Biology2e-WEB_eUNkk2q.pdf
+$homeurl/BusinessEthics-OP_BQNN2qf.pdf
+$homeurl/Calculus_Volume_1_-_WEB.pdf
+$homeurl/CalculusVolume2-OP_esPpXTB.pdf
+$homeurl/CalculusVolume3-OP_n7Nj74c.pdf
+$homeurl/Chemistry2e-OP_ZIUdkjH.pdf
+$homeurl/ChemistryAtomsFirst2e-OP_T2wT7wj.pdf
+$homeurl/CollegeAlgebra-OP_B7PUoNW.pdf
+$homeurl/College_Physics-WEB.pdf
+$homeurl/College_Success_web_slqpdO2.pdf
+$homeurl/Economics2e-OP_I2ne43X.pdf
+$homeurl/Entrepreneurship-WEB.pdf
+$homeurl/FinancialAccounting-OP_dwsQeqn.pdf
+$homeurl/IntermediateAlgebra2e-WEB.pdf
+$homeurl/IntroductionToBusiness-OP_gPQqj30.pdf
+$homeurl/IntroductionToSociology2e-OP_D8IepwZ.pdf
+$homeurl/IntroductoryStatistics-OP_LXn0jei.pdf
+$homeurl/Macroeconomics2e-OP.pdf
+$homeurl/ManagerialAccounting-OP_os574CR.pdf
+$homeurl/Microbiology-OP.pdf
+$homeurl/Microeconomics2e-OP_603oHEs.pdf
+$homeurl/OrganizationalBehavior-OP.pdf
+$homeurl/Prealgebra2e-WEB_yC5bGuI.pdf
+$homeurl/Precalculus-OP_mbNC8an.pdf
+$homeurl/Prealgebra2e-WEB_yC5bGuI.pdf
+$homeurl/Psychology-OP_F4YYkDr.pdf
+$homeurl/USHistory-OP_IdjNctE.pdf
+$homeurl/UniversityPhysicsVolume1-OP_gUzvYbP.pdf
+$homeurl/UniversityPhysicsVolume2-OP.pdf
+$homeurl/UniversityPhysicsVolume3-OP.pdf
+EOD
+
         pause
 }
 
